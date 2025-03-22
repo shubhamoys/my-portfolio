a3e39d769a9361d151fe5c4ea686a2af6b67c32f
Shubhamoy <shubhamoys@gmail.com>
2025-03-23 01:09:16 +0530

    ~ Enhancement: About section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import About from "@/components/sections/about/about";
+import Contact from "@/components/sections/contact/contact";
+import Experience from "@/components/sections/experience/experience";
 import Hero from "@/components/sections/hero/hero";
+import TechStack from "@/components/sections/tech-stack/tech-stack";
 
 export default function Home() {
   return (
@@ -10,6 +13,18 @@ export default function Home() {
 
       <section id="about">
         <About></About>
+      </section>
+
+      <section id="tech-stack">
+        <TechStack></TechStack>
+      </section>
+
+      <section id="experience">
+        <Experience></Experience>
+      </section>
+
+      <section id="contact">
+        <Contact></Contact>
       </section>
     </>
   );
---
+++ src/components/sections/tech-stack/tech-stack.tsx
@@ -0,0 +1,13 @@
+import styles from "./tech-stack.module.scss";
+
+export default function TechStack() {
+  return (
+    <div>
+      <div className={styles.container}>
+        <div className={styles.sectionTitle}>
+          <span className="body3-medium">Tech Stack</span>
+        </div>
+      </div>
+    </div>
+  );
+}
